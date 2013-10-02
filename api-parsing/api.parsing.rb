c8a14960f102168606a552da9803108d122b2cc2
Logan Hasson <logan.hasson@gmail.com>
2013-10-02 10:38:28 -0400

    Removed supurfluous code from api.parsing.rb

--- api-parsing/api.parsing.rb
+++ api-parsing/api.parsing.rb
@@ -9,14 +9,6 @@ reddit_html = "                  "
 
 def make_post_html(post_hash)
   data_hash = post_hash["data"]
-  post_url = "<a href=\"http://reddit.com#{data_hash["permalink"]}\">"
-  header = "<h1>#{data_hash["title"]}</h1>"
-  img = "<img src=\"#{data_hash["thumbnail"]}\" />"
-  upvote_title = "<p>Upvotes:</p>"
-  num_votes_h4 = "<h4>#{data_hash["ups"]}</h4>"
-  down_title = "<p>Downvotes:</p>"
-  num_downs_h4 = "<h4>#{data_hash["downs"]}</h4>"
-  post_string = "<li>#{post_url}#{header}#{img}#{upvote_title}#{num_votes_h4}#{down_title}#{num_downs_h4}</a></li>"
   post_string2 = "<li>
                       <a href=\"http://reddit.com#{data_hash["permalink"]}\">
                         <h1>#{data_hash["title"]}</h1>
