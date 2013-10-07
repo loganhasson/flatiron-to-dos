df5fe0de730772b5e89c1e0bff15c5847b2de5db
Logan Hasson <logan.hasson@gmail.com>
2013-10-07 09:27:52 -0400

    Refactored using .select and .sort

--- anagram/anagram.rb
+++ anagram/anagram.rb
@@ -1,4 +1,3 @@
-require 'pry'
 class Anagram
 
   attr_accessor :word
@@ -7,18 +6,24 @@ class Anagram
     @word = word
   end
 
+  # def match(anagram_array)
+  #   anagram_array.map do |potential|
+  #     temp_potential = potential.clone
+  #     if potential.length == self.word.length
+  #       self.word.each_char do |c|
+  #         if potential.include?(c)
+  #           potential.sub!(c, "")
+  #         end
+  #       end
+  #       temp_potential if potential.length == 0
+  #     end
+  #   end.compact
+  # end
+
+  # REFACTORED
+
   def match(anagram_array)
-    anagram_array.map do |potential|
-      temp_potential = potential.clone
-      if potential.length == self.word.length
-        self.word.each_char do |c|
-          if potential.include?(c)
-            potential.sub!(c, "")
-          end
-        end
-        temp_potential if potential.length == 0
-      end
-    end.compact
+    anagram_array.select {|item| item.chars.sort == self.word.chars.sort}
   end
 
 end
