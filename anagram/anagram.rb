f85afa56513e0db51f50dc906692d4759af1f1f7
Logan Hasson <logan.hasson@gmail.com>
2013-10-07 08:57:43 -0400

    pass anagram_spec.rb

---
+++ anagram/anagram.rb
@@ -0,0 +1,24 @@
+require 'pry'
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(anagram_array)
+    anagram_array.map do |potential|
+      temp_potential = potential.clone
+      if potential.length == self.word.length
+        self.word.each_char do |c|
+          if potential.include?(c)
+            potential.sub!(c, "")
+          end
+        end
+        temp_potential if potential.length == 0
+      end
+    end.compact
+  end
+
+end
